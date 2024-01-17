f0381cf34157fe9f9ebff340b3c940e0df0479ca
PavloLysianyi <145061284+PavloLysianyi@users.noreply.github.com>
2024-01-17 18:35:21 +0200

    corrected

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,152 +1,28 @@
 import React from 'react';
-import { nanoid } from 'nanoid';
-
-class ContactForm extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: '',
-      number: '',
-    };
-  }
-
-  handleNameChange = event => {
-    this.setState({ name: event.target.value });
-  };
-
-  handleNumberChange = event => {
-    this.setState({ number: event.target.value });
-  };
-
-  handleAddContact = () => {
-    const { name, number } = this.state;
-    const { onAddContact, contacts } = this.props;
-
-    if (name.trim() === '' || number.trim() === '') {
-      alert("Будь ласка, введіть ім'я та номер контакту.");
-      return;
-    }
-
-    const isNameExists = contacts.some(contact => contact.name === name);
-
-    if (isNameExists) {
-      alert(`Контакт з ім'ям ${name} вже існує у телефонній книзі.`);
-      return;
-    }
-
-    const newContact = {
-      id: nanoid(),
-      name,
-      number,
-    };
-
-    onAddContact(newContact);
-    this.setState({
-      name: '',
-      number: '',
-    });
-  };
-
-  render() {
-    const { name, number } = this.state;
-
-    return (
-      <div className="form-container">
-        <label className="label">
-          Ім'я контакту:
-          <input
-            type="text"
-            name="name"
-            value={name}
-            onChange={this.handleNameChange}
-            required
-            className="input"
-          />
-        </label>
-        <label className="label">
-          Номер телефону:
-          <input
-            type="tel"
-            name="number"
-            value={number}
-            onChange={this.handleNumberChange}
-            required
-            className="input"
-          />
-        </label>
-        <button
-          type="button"
-          onClick={this.handleAddContact}
-          className="button"
-        >
-          Додати контакт
-        </button>
-      </div>
-    );
-  }
-}
-
-class Filter extends React.Component {
-  render() {
-    const { filter, onFilterChange } = this.props;
-
-    return (
-      <div>
-        Пошук контактів за ім'ям
-        <input
-          type="text"
-          value={filter}
-          onChange={onFilterChange}
-          className="input"
-        />
-      </div>
-    );
-  }
-}
-
-class ContactList extends React.Component {
-  render() {
-    const { contacts, onDeleteContact } = this.props;
-
-    return (
-      <ul className="list">
-        {contacts.map(contact => (
-          <li key={contact.id} className="list-item">
-            <span className="contact-info">
-              {contact.name} - {contact.number}
-            </span>
-            <button
-              type="button"
-              onClick={() => onDeleteContact(contact.id)}
-              className="delete-button"
-            >
-              Видалити
-            </button>
-          </li>
-        ))}
-      </ul>
-    );
-  }
-}
+import ContactForm from './ContactForm';
+import Filter from './Filter';
+import ContactList from './ContactList';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      contacts: [],
-      filter: '',
-    };
-  }
+  state = {
+    contacts: [],
+    filter: '',
+  };
 
   componentDidMount() {
     const storedContacts = localStorage.getItem('contacts');
+
     if (storedContacts) {
-      this.setState({ contacts: JSON.parse(storedContacts) });
+      this.setState({
+        contacts: JSON.parse(storedContacts),
+      });
     }
   }
 
-  componentDidUpdate() {
-    localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.contacts !== this.state.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
   }
 
   handleFilterChange = event => {
@@ -154,6 +30,18 @@ class App extends React.Component {
   };
 
   handleAddContact = newContact => {
+    const { name } = newContact;
+    const { contacts } = this.state;
+
+    const isNameExists = contacts.some(
+      contact => contact.name.toLowerCase() === name.toLowerCase()
+    );
+
+    if (isNameExists) {
+      alert(`Контакт з ім'ям ${name} вже існує у телефонній книзі.`);
+      return;
+    }
+
     this.setState(
       prevState => ({
         contacts: [...prevState.contacts, newContact],
